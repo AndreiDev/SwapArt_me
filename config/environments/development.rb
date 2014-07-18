8283689fd1fbf13d6f3dfddbdba1cdb46ef5764b
AndreiDev <andrei.averkin@gmail.com>
2014-07-18 18:33:16 +0300

    first devise commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,19 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Added after installing DEVISE gem
+  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => config.app_domain }
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: '587',
+      enable_starttls_auto: true,
+      user_name: 'andrei.averkin',
+      password: 'gsar5248',
+      authentication => :plain,
+      domain => 'swapart.com'
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+  # added after DEVISE installation
+  root 'home#index'
+
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+
+  match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
